dba7634749d651f585bc9c0abb5421cf110081d5
Eli Hirsch <eli.b.hirsch@gmail.com>
2023-02-21 19:09:31 +0000

    added block creation

---
+++ create_block.py
@@ -0,0 +1,11 @@
+from prefect.filesystems import GitHub
+
+def create_block():
+    block = GitHub(
+        repository="https://github.com/elijellyeli/data-zoomcamp.git"
+    )
+    block.get_directory("flows") # specify a subfolder of repo
+    block.save("zoom-github")
+
+if __name__ == "__main__":
+    create_block()
