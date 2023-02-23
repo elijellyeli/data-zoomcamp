94cba90c3843d3ad23ff4c2453344fca2b1dc664
eli.b.hirsch@gmail.com <eli.b.hirsch@gmail.com>
2023-02-23 14:22:42 +0000

    added schema and tests

--- dbt/models/staging/stg_green_tripdata.sql
+++ dbt/models/staging/stg_green_tripdata.sql
@@ -1,6 +1,14 @@
 
 {{ config(materialized="view") }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata_non_partitoned') }}
+  where vendorid is not null 
+)
+
 select
     -- identifiers
     {{ dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -32,11 +40,10 @@ select
     {{ get_payment_type_description('payment_type')  }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source("staging", "green_tripdata_non_partitoned") }}
-where vendorid is not null
+from tripdata
+where rn = 1
 
--- dbt build --m <model.sql> --var 'is_test_run: false'
-
+-- dbt build --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
   limit 100
 {% endif %}
